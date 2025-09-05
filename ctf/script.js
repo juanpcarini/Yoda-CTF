3150392fabfe4f57701b8a64d113c06eff1bd282
Juan Pablo Carini <74880626+juanpcarini@users.noreply.github.com>
2025-09-05 14:37:05 -0300

    Update script.js

--- ctf/script.js
+++ ctf/script.js
@@ -3,7 +3,13 @@ DB_PASSWORD=P@ssw0rd123
 JWT_SECRET=supersecrettoken
 REDIS_URL=redis://localhost:6379
 
-724f03541151001aa2c13852fdb1bf94de033a8bd092dca3667c994c13dd16dbaf4c0cdf8df593c42502641ab14c207ce9a4be699f6209b68455be8cbb4f1226
+"En bloques de dieciséis mi fuerza hallarás",
+"ni hash ni cifra eterna jamás serás.",
+"Con la llave correcta abrirme podrás",
+"sin ella en sombras siempre quedarás."
+
+
+"724f03541151001aa2c13852fdb1bf94de033a8bd092dca3667c994c13dd16dbaf4c0cdf8df593c42502641ab14c207ce9a4be699f6209b68455be8cbb4f1226"
 
 
 API_KEY=sk-fake-ABC123456789
